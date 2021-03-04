ce75b525e7420823531cdaa7a1dbcd127fc06cd7
Vera Baihuiqian He <hebaihuiqian.bo@gmail.com>
2021-03-04 15:18:31 +0800

    Add files via upload

---
+++ R_script_template.R
@@ -0,0 +1,17 @@
+# 1. prerequisite----
+## libraries----
+Packages <- c("tidyverse", # data wrangle
+              "raster",   # spatial
+              "openair")  # air quality 
+lapply(Packages, library, character.only = TRUE)
+
+
+## functions----
+# either define or import from previous script
+
+# 2.  import----
+
+# 3. analysis ----
+
+
+# 4. output----
